65d4fbba8cbfed07ba2ccfb7d5b2b512ed9d4368
Katherine Milagros <katherinebemol17@gmail.com>
2022-09-24 23:04:24 -0500

    Se corrigio la función MD-Links

--- cli.js
+++ cli.js
@@ -2,7 +2,3 @@
 
 /* const process = require('node:process');
 console.log(process.argv); */
-
-// Prueba de path
-const path = require('node:path');
-console.log(path);
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const fs = require('node:fs');
+const fs = require('node:fs'); // Importo de FS
 const path = require('node:path'); // Importo de path
 const axios = require('axios'); 
-const { link } = require('node:fs');
+const { link, Stats } = require('node:fs');
+const { Console } = require('node:console');
+const { resolve } = require('node:path');
 
 // 1. Función existe la ruta
 const existPath = (enteredPath) => {
@@ -13,68 +15,143 @@ const existPath = (enteredPath) => {
 const pathIsAbsolute = (enteredPath) => {
   return path.isAbsolute(enteredPath); 
 };
+/* console.log(pathIsAbsolute('E:/Laboratoria-Proyecto4/LIM018-md-links/cli.js')); */
 
-// 3. Función convierte la ruta a absoluta
+// 3. Función convierte la ruta a absoluta IMPORTANTE
 const convertToAbsolute = (enteredPath) => {
   return pathIsAbsolute(enteredPath) ? enteredPath : path.resolve(enteredPath);
 };
-//console.log(convertToAbsolute('cli.js'));
+/* console.log(convertToAbsolute('cli.js')); */
 
-// 4. Función para saber el tipo de extensión del archivo
+// 4. Función para saber sí es un archivo
+const isFile = (enteredPath) => {
+  return fs.statSync(enteredPath).isFile();
+}
+/* console.log('es un archivo')
+console.log(isFile('E:/Laboratoria-Proyecto4/LIM018-md-links/dir-prueba1/archi-prueba1.md'));
+console.log(isFile('dir-prueba1')); */
+
+// 5. Función para saber el tipo de extensión del archivo
 const tipeOfExtension = (enteredPath) => {
-  return path.extname(enteredPath);
+  return path.extname(enteredPath) === '.md';
 }
-/* console.log(tipeOfExtension('E:/Laboratoria-Proyecto4/LIM018-md-links/archivos-prueba/PRUEBA1.md')); */
+/* console.log('esto es un prueba');
+console.log(tipeOfExtension('E:/Laboratoria-Proyecto4/LIM018-md-links/dir-prueba1/archi-prueba1.txt')); */
 
-// 5. Función para leer el archivo md
+// 6. Función para leer el archivo md IMPORTANTE
 const readFile = (file) => {
-  return fs.readFileSync(file, 'utf-8'); 
+  if(tipeOfExtension(file)){
+    return fs.readFileSync(file, 'utf-8'); 
+  } else {
+    throw ('No se encontraron archivos con extensión .md')
+  }
 }
+/* console.log(readFile('E:/Laboratoria-Proyecto4/LIM018-md-links/dir-prueba1/archi-prueba1.md', 'utf-8')); */
+ 
 
-// 6. Función para saber si el archivo md incluye links
-const fileInfomation = (enteredFile) => {
-  const regularExpretion = /\[([^\[]+)\](\(https:.*\))/gm;
-  const readFile = fs.readFileSync(enteredFile, 'utf-8');
+// 7. Función para leer un directorio
+const readDir = (dir) => {
+  return fs.readdirSync(dir);
+}
+/* console.log(readDir('dir-prueba1' , 'leer un directorio')); */
+
+// 8. Función para saber sí mi archivo incluye links .md
+const getLinksMD = (enteredFile) => { // entra 1 archivo
+  const rexExpretion = /(\[(.*?)\])?\(http(.*?)\)/gm;
   const arrayOfLinks = [];
 
-  const foundLinks = readFile.match(regularExpretion);
+  const foundLinks = readFile(enteredFile).match(rexExpretion);
+  // 
+  if(foundLinks === null){
+    console.log('No se encontraron links');
+    return [];
+  }
 
-  if (foundLinks) {
-    foundLinks.forEach((link) => {
-      const separator = link.indexOf(']'); // encuentra el 1er elem q coincida
-      // lo que tiene que estar en mi objeto
-      const href = link.slice(separator + 2, link.length - 1);
-      const text = link.slice(1, separator); // hasta el txtRef sin incluirme el 
-      const file = enteredFile;
+  foundLinks.forEach((link) => {
+    const separator = link.indexOf(']'); // encunetra el 1er elem q coincida
+    // info que tiene que estar en mi array de obj
+    const href = link.slice(separator + 2 , link.length - 1); // desde - hasta fin (sin incluirme el fin)
+    const text = link.slice(1, separator);
+    const file = enteredFile;
 
-      arrayOfLinks.push({ href, text, file });
-    });
-  };
+    arrayOfLinks.push({href, text, file});
+  });
   return arrayOfLinks;
-}
-/* console.log(fileInfomation('archivos-prueba/PRUEBA1.md'));  */ // es 1 obj con la información del archivo
+}  
+/* console.log(getLinksMD('E:/Laboratoria-Proyecto4/LIM018-md-links/dir-prueba1/archi-prueba1.md')); 
+console.log(getLinksMD('E:/Laboratoria-Proyecto4/LIM018-md-links/dir-prueba2/otro-archi.txt')); */
 
-
-// 7. Función para validar status de los link con petición HTTP
-const validateLinksStatus = (arrayOfLinks) => {
-  return Promise.all(arrayOfLinks.map((link) => {
-    return axios.get(link.href) // queremos hacer 1 petición a esta dirección
-
-    .then((response) => {
+// 9. Función para validar status de los links del archivo con petición HTTP
+const validateLinkStatus = (path) => { // le paso 1 ruta
+  const arrayOfObj = getLinksMD(path); // le paso 1 array de obj
+  return Promise.all(arrayOfObj.map((link) => {
+    return axios.get(link.href) // quiero hacer 1 petición HTTP a esta dirección
+      .then((response) => {
       link.status = response.status;
       link.message = response.statusText;
       return link;
     })
-    
     .catch((error) => {
       link.status = error.response.status;
-      link.message = 'fail';
+      link.message = 'Fail';
       return link;
-    })
-
+     })
   }))
-}  
-validateLinksStatus(fileInfomation('./archivos-prueba/PRUEBA1.md')).then((response) => console.log(response));
+}
+/* console.log(validateLinkStatus(getLinksMD('E:/Laboratoria-Proyecto4/LIM018-md-links/dir-prueba1/archi-prueba1.md')).then((response) => {
+  console.log(response)
+})); */
+
+
+// 11. Función recursiva entrar al directorio y extraer los archivos
+const obtainDirAndFiles = (enteredPath) => {
+  const arrayOfFiles = [];
+  if(isFile(enteredPath)){
+    return [enteredPath]
+  } else {
+    const readDirectory = readDir(enteredPath);
+
+    readDirectory.forEach((dir) => {
+      const newPath = path.join(enteredPath, dir);
+      arrayOfFiles.push(obtainDirAndFiles(newPath));
+    });
+    return arrayOfFiles.flat();
+  }
+}
+
+/* console.log('recursión');
+console.log(obtainDirAndFiles('dir-prueba1')); 
+console.log(obtainDirAndFiles('E:/Laboratoria-Proyecto4/LIM018-md-links/dir-prueba1/archi-prueba1.md')); */
+
+// 12. Función MD-LINKS
+const mdLinks = (path, options) =>{
+  return new Promise ((resolve, reject) => {
+    if(!existPath(path)){
+      reject(new Error('No existe la ruta, ingrese una ruta valida'));
+    }
+    //2. ¿El tipo de ruta es absoluta?
+    const absolutePath = convertToAbsolute(path); // convierte la ruta a abs
+    /* console.log(absolutePath , 'Y tú');  */
+    const arrayOfFilesPaths = obtainDirAndFiles(absolutePath);
+    arrayOfFilesPaths.forEach((path) => {
+      if(options.validate === true){
+        resolve(validateLinkStatus(path))
+        .then((response) => {
+          console.log(response)
+        })
+      }else{
+        resolve(getLinksMD(path))
+      }
+    }); 
+  });
+};
+
+mdLinks('./dir-prueba2', {validate:true}).then(console.log).catch(console.error); 
+/* mdLinks('E:/Laboratoria-Proyecto4/LIM018-md-links/dir-prueba1/archi-prueba1.md', {validate:true})
+.then(console.log)
+.catch(console.error); */
+
+
 
 module.exports = {
   existPath,
@@ -82,6 +159,4 @@ module.exports = {
   convertToAbsolute,
   tipeOfExtension,
   readFile,
-  fileInfomation,
-  validateLinksStatus,
 };
